794f04436f590f696274aa9257553567adb985d3
AndresJaramillo2023 <131599864+AndresJaramillo2023@users.noreply.github.com>
2023-08-17 21:49:44 -0500

    Update client.controller.js

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,4 +1,4 @@
-import { clientServices } from "services/client-service.js";
+import { clientServices } from "../services/client-service.js";
 
 const crearNuevaLinea = (url, categoria, nombre, precio, descripcion, id) => {
   const linea = document.createElement("div");
